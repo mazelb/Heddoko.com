ab45422202bc26692998e4f2078cae7005f59632
Nick Woodhams <nicholas.woodhams@gmail.com>
2016-03-15 03:17:03 -0400

    replaced mailchimp footer opt-in

--- includes/index/subscribe.php
+++ includes/index/subscribe.php
@@ -1,22 +1,33 @@
 <section id="subscribe">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-				<h2>Subscribe now and get exclusive <span>updates and discounts!</span></h2>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-				<form class="form" action="http://heddoko.us8.list-manage.com/subscribe/post-json?u=4752cc710a498b9b75d697500&amp;id=b22a426cab&amp;c=?" method="get" id="mc-embedded-subscribe-form-2" name="mc-embedded-subscribe-form" class="validate" novalidate="">
-					<div class="input-group">
-						<input type="email" value="" name="EMAIL" id="mce-EMAIL-2" placeholder="Email" class="form-control mce-EMAIL-class" required="">
-						<div style="position: absolute; left: -5000px;"><input type="text" name="b_1e97093fbabd802e86fdd5516_09ef45aaaa" tabindex="-1" value=""></div>
-						<span class="input-group-btn">
-							<input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe-2" class="button btn btn-default">
-						</span>
-					</div>
-					<div id="thankyoudiv-2"><h3>Thank You!</h3></div>
-					<img src="images/ajax-loader-ongreen.gif" class="ajax">
-				</form>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h2>Subscribe now and get exclusive <span>updates and discounts!</span></h2>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                    <form action="//heddoko.us8.list-manage.com/subscribe/post?u=4752cc710a498b9b75d697500&amp;id=b22a426cab" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <div class="mc-field-group input-group">
+                                <input type="email" value="" name="EMAIL" placeholder="Email" class="required email form-control mce-EMAIL-class" id="mce-EMAIL">
+                                <span class="input-group-btn">
+				    	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-default">
+				    </span>
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                <input type="text" name="b_4752cc710a498b9b75d697500_b22a426cab" tabindex="-1" value="">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!--End mc_embed_signup-->
+            </div>
+        </div>
+    </div>
 </section>
