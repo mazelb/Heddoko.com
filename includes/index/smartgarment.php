4f9eeebb35576938912ce45d83f7d211fc3e737d
Nick Woodhams <nicholas.woodhams@gmail.com>
2016-03-23 02:30:05 -0400

    added new logos, links, rearranged sections, updated video link, started updating product page, etc

---
+++ includes/index/smartgarment.php
@@ -0,0 +1,48 @@
+<section id="productspecs" itemscope itemtype="http://schema.org/MobileApplication" class="clearfix">
+    <meta itemprop="operatingSystem" content="iOS">
+    <meta itemprop="applicationCategory" content="Wearables, Sports">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 content">
+                <h2 data-animated="0">Smart Garment &amp; App (Redesign)<span></span></h2>
+                <p>Our system brings together a featherweight compression suit that fits like a second skin, small scale sensors that track your movement, and an application that models those movements real‐time in 3D and provides actionable insights.</p>
+                <p prop="about" data-animated="0">Heddoko empowers perfection in human movement, and provides real‐time 3D corrective feedback.</p>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-dark-sensors.svg)" title="Integrated sensors"></div>
+                        <h4>Integrated<br> sensors</h4>
+                    </div>
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-dark-body.svg)" title="Full-body data capture"></div>
+                        <h4>Full-body<br> data capture</h4>
+                    </div>
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-dark-bluetooth.svg)" title="Resists tough training"></div>
+                        <h4>Wireless and<br> camera-less</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-dark-forceapplied.svg)" title="Live feedback"></div>
+                        <h4>Live feedback</h4>
+                    </div>
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-dark-recording-mov.svg)" title="Movement Recording"></div>
+                        <h4>Movement<br> Recording</h4>
+                    </div>
+                    <div class="col-xs-12 col-sm-4" data-animated="0">
+                        <div class="has-icon-img" style="background-image: url(images/icons/icon-injury.svg)" title="Complete injury profile"></div>
+                        <h4>Movement<br> Analysis</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 content">
+                <img itemprop="image" src="images/index/garment.png" data-animated="4" alt="Garment" class="animated-in">
+                <img itemprop="screenshot" src="/images/index/ipad-dashboard.png" alt="App" class="ipad-dashboard" />
+            </div>
+        </div>
+        <div class="button-container">
+            <a itemprop="url" href="/product/#mobile" class="btn btn-default" id="index_pg_mobile_learnmore_btn">Learn more</a>
+        </div>
+    </div>
+</section>
--- js/index.js
+++ js/index.js
@@ -493,7 +493,7 @@ $(document).ready(function() {
 	/**
 	* Init
 	*/
-	resetHeight();
+	// resetHeight();
 
 });
 
