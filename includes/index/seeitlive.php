4dd0bb03c687b85bfc2519d42d26bdfe3e4d3f69
Nick Woodhams <nicholas.woodhams@gmail.com>
2016-03-29 19:47:23 -0400

    updated video link

--- includes/index/seeitlive.php
+++ includes/index/seeitlive.php
@@ -1,7 +1,7 @@
 <section id="seeitlive" class="clearfix">
     <div class="container">
         <h1>See it Live</h1>
-        <a href="https://youtu.be/VDpP90SaNBI" target="_blank" class="btn play-button btn-default-outline">Watch Now</a>
+        <a href="https://www.youtube.com/watch?v=8sjTdvc6Bfk&feature=youtu.be" target="_blank" class="btn play-button btn-default-outline">Watch Now</a>
         <p>Visit our Youtube channel for more <a href="https://www.youtube.com/channel/UCvOdDgvdvEvhJKcAtpX4Kdg" target="_blank" id="index_pg_seeitlive_btn">videos</a>.</p>
     </div>
     <!--<div class="darken"></div>-->
