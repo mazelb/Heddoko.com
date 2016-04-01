d138752067241831b4c559c5b74a71b3a5373854
Nick Woodhams <nicholas.woodhams@gmail.com>
2016-04-01 06:20:07 -0400

    finished first pass of mobile optimization for all pages

--- includes/ergonomics/howitworks.php
+++ includes/ergonomics/howitworks.php
@@ -7,53 +7,53 @@
 				<p>Completely mobile. Easy to set up. Easy to use. Data that is easy to interpret.</p>
 
 				<div class="row select active" data-animated="0">
-					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
 						<span class="image" title="Motion capture (Mocap) suit">
 							<img src="../images/icons/icon-suit-basic.svg" alt="Suit up with Heddoko">
 						</span>
 
 						<span class="vertical"></span>
 					</div>
-					<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 step-text">
+					<div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 step-text">
 						<h4>Suit up with Heddoko</h4>
 					</div>
 				</div>
 
 				<div class="row select" data-animated="0">
-					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
 						<span class="image" title="Mobile coaching">
 							<img src="../images/icons/icon-heddokoapp-tablet-basic.svg" alt="Pair with Mobile/Web App">
 						</span>
 
 						<span class="vertical"></span>
 					</div>
-					<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 step-text">
+					<div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 step-text">
 						<h4>Pair with Application</h4>
 					</div>
 				</div>
 
 				<div class="row select" data-animated="0">
-					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
 						<span class="image" title="Movement analysis">
 							<img src="../images/icons/icon-3dmovement-basic.svg" alt="3D motion capture">
 						</span>
 
 						<span class="vertical"></span>
 					</div>
-					<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 step-text">
+					<div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 step-text">
 						<h4>Capture and analyze movement live in 3D</h4>
 					</div>
 				</div>
 
 				<div class="row select" data-animated="0">
-					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
 						<span class="image" title="Improve sports performance">
 							<img src="../images/icons/icon-improve-basic.svg" alt="Movement perfection">
 						</span>
 
 						<span class="vertical"></span>
 					</div>
-					<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 step-text">
+					<div class="col-xs-8 col-sm-9 col-md-9 col-lg-9 step-text">
 						<h4>Make immediate adjustments to reduce risk of injury</h4>
 					</div>
 				</div>
